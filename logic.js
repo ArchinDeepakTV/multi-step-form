b4fd9443d1ab91d7c5a5c479e4f14cc6a8520160
ADeepakTV <archindeepakad.ad@gmail.com>
2023-08-14 16:42:15 +0530

    exported gathered data

--- logic.js
+++ logic.js
@@ -5,4 +5,10 @@ function logging() {
   console.log(customerName.value);
   console.log(phNo.value);
   console.log(bikeModel.value);
+  var customerDetails = {
+    name: customerName.value,
+    phone: phNo.value,
+    model: bikeModel.value,
+  };
+  module.exports = { customerDetails };
 }
