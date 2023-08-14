4ef5ec609f33d2cf4a9e820de1b42d6bfb6e5e2c
ADeepakTV <archindeepakad.ad@gmail.com>
2023-08-14 16:18:53 +0530

    home page made

---
+++ logic.js
@@ -0,0 +1,8 @@
+function A() {
+  const name = document.getElementById("name");
+  const phNo = document.getElementById("phone-number");
+  const bikeModel = document.getElementById("bikes");
+  console.log(name.value);
+  console.log(phNo.value);
+  console.log(bikeModel.value);
+}
