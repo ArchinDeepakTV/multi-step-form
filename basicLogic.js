32c7ef4b4d4558c865c26b054bc86f30db631dba
ADeepakTV <archindeepakad.ad@gmail.com>
2023-08-17 10:37:54 +0530

    made basic webpage with grid showcasing data gathered on previous site and image corresponding to the selected model

--- basicLogic.js
+++ basicLogic.js
@@ -7,7 +7,7 @@ const cName = reverseCipher(tempName);
 console.log(cName);
 const phNo = urlParams.get("ph");
 console.log(phNo);
-const model = urlParams.get("model");
+var model = urlParams.get("model");
 console.log(model);
 
 function reverseCipher(customerName) {
@@ -25,3 +25,26 @@ function reverseCipher(customerName) {
   // console.log(cName);
   return cName;
 }
+
+if (model == "gt650") {
+  model = "Royal Enfield Continental GT 650";
+  document.getElementById("imgs").src = "./assets/img/GT650.jpg";
+} else if (model == "classic350") {
+  model = "Royal Enfield Classic 350";
+  document.getElementById("imgs").src = "./assets/img/classic350.jpg";
+} else if (model == "thunderbird") {
+  model = "Royal Enfield Thunderbird";
+  document.getElementById("imgs").src = "./assets/img/thunderbird.jpg";
+} else if (model == "meteor") {
+  model = "Royal Enfield Meteor 350";
+  document.getElementById("imgs").src = "./assets/img/meteor.jpg";
+} else if (model == "himalayan") {
+  model = "Royal Enfield Himalayan 350";
+  document.getElementById("imgs").src = "./assets/img/himalayan.jpg";
+} else if (model == "interceptor650") {
+  model = "Royal Enfield Interceptor GT 650";
+  document.getElementById("imgs").src = "./assets/img/interceptor650.jpg";
+}
+document.getElementById("cName").placeholder = cName.toUpperCase();
+document.getElementById("phone-number").placeholder = phNo;
+document.getElementById("model").placeholder = model.toUpperCase();
